8b29d34857a52cd6c7f0356aac17719d4e2d5348
ARZarezadeh <83907952+ARZarezadeh@users.noreply.github.com>
2025-02-04 15:32:36 +0330

    activity_monitor -> add show_more/show_less button to dropdown

--- custom_addons/activity_monitor/static/src/js/systray_activity_monitor_menu.js
+++ custom_addons/activity_monitor/static/src/js/systray_activity_monitor_menu.js
@@ -13,11 +13,14 @@ let ActivityMonitorMenu = Widget.extend({
         'click .o_activity_monitor_activity_monitor_action': '_onActivityMonitorActionClick',
         'click .o_last_changes_preview': '_onLastChangesRecordClick',
         'show.bs.dropdown': '_onActivityMonitorMenuShow',
-        'hide.bs.dropdown': '_onActivityMonitorMenuHide'
+        'hide.bs.dropdown': '_onActivityMonitorMenuHide',
+        'click .o_show_more_changes': '_onShowMoreChanges',
+        'click .o_show_less_changes': '_onShowLessChanges',
     },
     start: function () {
         this._$activityMonitorPreview = this.$('.o_activity_monitor_systray_dropdown_items');
         this._updateActivityMonitorPreview();
+        this._showAllChanges = false;
         return this._super();
     },
 
@@ -34,7 +37,8 @@ let ActivityMonitorMenu = Widget.extend({
             args: [],
             kwargs: {context: session.user_context},
         }).then(function (data) {
-            self._changes = data;
+            self._allChanges = data.sort((a, b) => new Date(b.write_date) - new Date(a.write_date))
+            self._changes = !self._showAllChanges ? self._allChanges.slice(0, 10) : self._allChanges
             self._changeCounter = data.length
             self.$('.o_last_changes_counter').text(self._changeCounter);
         });
@@ -121,6 +125,21 @@ let ActivityMonitorMenu = Widget.extend({
             });
         }
     },
+
+     _onShowMoreChanges: function (ev) {
+        ev.stopPropagation();
+        this._showAllChanges = true;
+        // this._changes = this._allChanges; // Show all changes
+        //  console.log("this._changes: ", this._changes)
+        this._updateActivityMonitorPreview();
+    },
+
+    _onShowLessChanges: function (ev) {
+        ev.stopPropagation();
+        this._showAllChanges = false;
+        // this._changes = this._allChanges.slice(0, 10);  // Show only 10
+        this._updateActivityMonitorPreview();
+    }
 });
 
 systrayMenu.Items.push(ActivityMonitorMenu);
